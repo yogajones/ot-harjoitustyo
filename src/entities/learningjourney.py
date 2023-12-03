8efcc60883eeb9dea8db6af280ab4daa6cbc584e
yogajones <95644662+yogajones@users.noreply.github.com>
2023-12-03 16:40:04 +0200

    Fix: harmonize entities with database

--- src/entities/learningjourney.py
+++ src/entities/learningjourney.py
@@ -1,6 +1,7 @@
 class LearningJourney:
 
-    def __init__(self, name: str, active=1) -> None:
+    def __init__(self, name: str, active=1, id=None) -> None:
         """Creates a new, active Learning Journey."""
+        self.id = id
         self.name = name
         self.active = active
--- src/entities/objective.py
+++ src/entities/objective.py
@@ -1,5 +1,6 @@
 class Objective:
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, lj_id=None) -> None:
         """Creates a new Objective."""
         self.name = name
+        self.lj_id = lj_id
