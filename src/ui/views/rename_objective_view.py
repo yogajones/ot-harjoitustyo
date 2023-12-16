a4610b0254286bea1625ba3809eaeb399ede5da2
yogajones <95644662+yogajones@users.noreply.github.com>
2023-12-16 12:41:29 +0200

    Refactor: apply refactoring to RenameObjectiveView

--- src/ui/views/rename_objective_view.py
+++ src/ui/views/rename_objective_view.py
@@ -5,33 +5,26 @@ from .base_view import BaseView
 
 class RenameObjectiveView(BaseView):
     def __init__(self, root, show_manage_view, selected_journey, current_id, current_name):
-        super().__init__(root)
         self._show_manage_view = show_manage_view
         self._selected_journey = selected_journey
+        self._current_name = current_name
+        self._current_id = current_id
 
-        self._rename_form(current_id, current_name)
-        self._return_to_manage_view()
+        super().__init__(root)
+        self._rename_form()
 
-    def _rename_form(self, current_id, current_name):
-        self._rename_label = ttk.Label(
-            self._frame, text=f"Rename {current_name}")
-        self._rename_label.pack()
+    def _rename_form(self):
+        self._new_name_entry = self._form_one_entry(f"Rename {self._current_name}",
+                                                    "Save",
+                                                    self._handle_rename_objective)
 
-        self._new_name_entry = ttk.Entry(self._frame)
-        self._new_name_entry.pack()
-
-        self._save_button = ttk.Button(
-            self._frame, text="Save",
-            command=lambda: self._handle_rename_objective(current_id))
-        self._save_button.pack()
-
-    def _handle_rename_objective(self, current_id):
+    def _handle_rename_objective(self):
         new_name = self._new_name_entry.get()
         if new_name:
-            objective_service.rename_objective(current_id, new_name)
+            objective_service.rename_objective(self._current_id, new_name)
             self._show_manage_view(self._selected_journey)
 
-    def _return_to_manage_view(self):
+    def _return_button(self):
         self._return_btn = ttk.Button(
             self._frame, text="Back",
             command=lambda: self._show_manage_view(self._selected_journey))
